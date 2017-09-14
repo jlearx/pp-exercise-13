62cd61585aeaf42a96c90465e215df7106d81431
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-09-14 11:11:02 -0700

    First commit; Added user input prompt

---
+++ src/main.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 14, 2017
+
+@author: jlearx
+'''
+
+def GetFibonacci(count):
+    fibonacci = []
+    return fibonacci
+
+if __name__ == '__main__':
+    print("Fibonacci Generator")
+    count = int(input("How many Fibonacci numbers would you like? "))
+    
+    fibonacci = GetFibonacci(count)
+    print(fibonacci)
+    
